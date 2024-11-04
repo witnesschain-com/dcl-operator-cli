b4d8741fccc4ebb67f6cdea5548346b90fe95ce2
Kripa Shanker <kripa.shanker@kaleidoscope-blockchain.com>
2024-11-04 11:32:58 +0530

    cleanup: combine constants and utils

--- common/common.go
+++ common/common.go
@@ -2,7 +2,11 @@ package common
 
 import (
 	"math/big"
+	"github.com/witnesschain-com/dcl-operator-cli/common/bindings/ChallengerRegistry"
+	"github.com/witnesschain-com/dcl-operator-cli/common/bindings/ProverRegistry"
+	op_common "github.com/witnesschain-com/operator-cli/common"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -59,4 +63,16 @@ var NetworkConfig = map[string]ChainConfig{
 	"17000":      Holesky,
 	"1702448187": WitnesschainMainnet,
 	"1":          EthMainnet,
+}
+
+func IsProverRegistered(prover common.Address, operator common.Address, proverRegistry *ProverRegistry.ProverRegistry) bool {
+	registered, err := proverRegistry.IsRegisteredProver(&bind.CallOpts{}, prover, operator)
+	op_common.CheckError(err, "Error checking if watchtower is already registered")
+	return registered
+}
+
+func IsValidChallenger(challenger common.Address, challengerRegistry *ChallengerRegistry.ChallengerRegistry) bool {
+	valid, err := challengerRegistry.IsValidChallenger(&bind.CallOpts{}, challenger)
+	op_common.CheckError(err, "Error checking if challenger is already registered")
+	return valid
 }
